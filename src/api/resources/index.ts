b814a6bc75ccb42c8eb19adaad8051d2595d4774
Sebastian Penhouet <s.penhouet@outlook.de>
2023-08-26 22:37:54 +0200

    Small oas info corrections

--- src/api/resources/index.ts
+++ src/api/resources/index.ts
@@ -6,9 +6,8 @@ export const RESOURCES = new JsonResources(
         openapi: "3.1.0",
         info: {
             title: "bcked API",
-            summary: "Free API for all data on bcked.com",
             // TODO Multiline description.
-            description: "",
+            description: "Free API for all data on bcked.com",
             termsOfService: "https://github.com/bcked/bcked-adapters/blob/main/LEGAL_NOTICE.md",
             contact: {
                 name: "API Support",
@@ -16,8 +15,7 @@ export const RESOURCES = new JsonResources(
                 email: "contact@bcked.com",
             },
             license: {
-                name: "GNU General Public License v3.0 only",
-                identifier: "GPL-3.0",
+                name: "GPL-3.0 license",
                 url: "https://github.com/bcked/bcked-adapters/blob/main/LICENSE",
             },
             // TODO maybe use the current commit hash here
