632c492468b6b8aedd7e14184007ed52a414bd78
GitHub Action <action@github.com>
2024-03-04 22:29:04 +0000

    Deploy new build

--- src/api/resources/systems.js
+++ src/api/resources/systems.js
@@ -35,8 +35,93 @@ var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, 
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SYSTEM_RESOURCES = void 0;
+const time_1 = require("../../utils/time");
 const icons_1 = require("../utils/icons");
 const resources_1 = require("../utils/resources");
+/**
+ * Converts the statistics object to a summary object.
+ * @param path - The path to set in the summary object, this includes which parts of the date to include e.g. `${path}/{year}/{month}/{day}/{hour}`.
+ * @param stats - The statistics object containing min, max, and median values.
+ * @returns The summary object with low, median, and high values.
+ * @throws Error if the stats object is missing min, max, or median values.
+ */
+function statsToSummary(path, stats) {
+    if (!stats.min || !stats.max || !stats.median) {
+        throw new Error("Stats missing. This should have been checked prior.");
+    }
+    return {
+        low: {
+            $ref: (0, time_1.setDateParts)(path, stats.min.timestamp),
+        },
+        median: {
+            $ref: (0, time_1.setDateParts)(path, stats.median.timestamp),
+        },
+        high: {
+            $ref: (0, time_1.setDateParts)(path, stats.max.timestamp),
+        },
+    };
+}
+function historyResource(path, latestTimestamp, stats, years, dateParts = "{year}/{month}/{day}/{hour}") {
+    if (!latestTimestamp || !stats || !stats.min || !stats.max || !stats.median || !years.length)
+        return;
+    return {
+        $id: path,
+        latest: {
+            $ref: (0, time_1.setDateParts)(`${path}/${dateParts}`, latestTimestamp),
+        },
+        history: {
+            ...statsToSummary(`${path}/${dateParts}`, stats),
+            data: years.map((year) => ({
+                $ref: `${path}/${year}`,
+            })),
+        },
+    };
+}
+function yearResource(path, stats, year, months, dateParts = "{year}/{month}/{day}/{hour}") {
+    if (!year || !months.length)
+        return;
+    if (!stats?.min || !stats.max || !stats.median)
+        return;
+    return {
+        $id: `${path}/${year}`,
+        ...statsToSummary(`${path}/${dateParts}`, stats),
+        data: months.map((month) => ({
+            $ref: `${path}/${year}/${month}`,
+        })),
+    };
+}
+function monthResource(path, stats, year, month, days, dateParts = "{year}/{month}/{day}/{hour}") {
+    if (!year || !month || !days.length)
+        return;
+    if (!stats?.min || !stats.max || !stats.median)
+        return;
+    return {
+        $id: `${path}/${year}/${month}`,
+        ...statsToSummary(`${path}/${dateParts}`, stats),
+        data: days.map((day) => ({
+            $ref: `${path}/${year}/${month}/${day}`,
+        })),
+    };
+}
+function dayResource(path, stats, year, month, day, hours, dateParts = "{year}/{month}/{day}/{hour}") {
+    if (!year || !month || !day || !hours.length)
+        return;
+    if (!stats?.min || !stats.max || !stats.median)
+        return;
+    return {
+        $id: `${path}/${year}/${month}/${day}`,
+        ...statsToSummary(`${path}/${dateParts}`, stats),
+        data: hours.map((hour) => ({
+            $ref: `${path}/${year}/${month}/${day}/${hour}`,
+        })),
+    };
+}
+function hourBaseResource(path, timestamp) {
+    return {
+        $id: (0, time_1.setDateParts)(`${path}/{year}/{month}/{day}/{hour}`, timestamp),
+        timestamp,
+    };
+}
 let System = (() => {
     var _a;
     let _instanceExtraInitializers = [];
@@ -45,6 +130,11 @@ let System = (() => {
     let _details_decorators;
     let _icons_decorators;
     let _assets_decorators;
+    let _totalValueLockedHistory_decorators;
+    let _totalValueLockedYear_decorators;
+    let _totalValueLockedMonth_decorators;
+    let _totalValueLockedDay_decorators;
+    let _totalValueLockedHour_decorators;
     return _a = class System extends resources_1.JsonResources {
             constructor() {
                 super({
@@ -77,6 +167,9 @@ let System = (() => {
                     assets: {
                         $ref: `/systems/${id}/assets`,
                     },
+                    "total-value-locked": {
+                        $ref: `/systems/${id}/total-value-locked`,
+                    },
                 };
             }
             async details(id, details) {
@@ -99,6 +192,28 @@ let System = (() => {
                     assets: assetIds.map((assetId) => ({
                         $ref: `/assets/${assetId}`,
                     })),
+                };
+            }
+            async totalValueLockedHistory(id, latestTimestamp, stats, years) {
+                return historyResource(`/systems/${id}/total-value-locked`, latestTimestamp, stats, years);
+            }
+            async totalValueLockedYear(id, stats, year, months) {
+                return yearResource(`/systems/${id}/total-value-locked`, stats, year, months);
+            }
+            async totalValueLockedMonth(id, stats, year, month, days) {
+                return monthResource(`/systems/${id}/total-value-locked`, stats, year, month, days);
+            }
+            async totalValueLockedDay(id, stats, year, month, day, hours) {
+                return dayResource(`/systems/${id}/total-value-locked`, stats, year, month, day, hours);
+            }
+            async totalValueLockedHour(id, stats) {
+                if (!stats?.min || !stats.max || !stats.median)
+                    return;
+                return {
+                    ...hourBaseResource(`/systems/${id}/total-value-locked`, stats.median.timestamp),
+                    value: {
+                        "rwa:USD": stats.median.totalValueLocked,
+                    },
                 };
             }
         },
@@ -143,11 +258,56 @@ let System = (() => {
                     // TODO write schema
                     schema: {},
                 })];
+            _totalValueLockedHistory_decorators = [resources_1.JsonResources.register({
+                    path: "/systems/{id}/total-value-locked",
+                    summary: "Get total value locked of an asset",
+                    description: "Get total value locked of an asset by its ID",
+                    type: "SystemTotalValueLocked",
+                    // TODO write schema
+                    schema: {},
+                })];
+            _totalValueLockedYear_decorators = [resources_1.JsonResources.register({
+                    path: "/systems/{id}/total-value-locked/{year}",
+                    summary: "Get total value locked of an asset",
+                    description: "Get total value locked of an asset by its ID",
+                    type: "SystemTotalValueLocked",
+                    // TODO write schema
+                    schema: {},
+                })];
+            _totalValueLockedMonth_decorators = [resources_1.JsonResources.register({
+                    path: "/systems/{id}/total-value-locked/{year}/{month}",
+                    summary: "Get total value locked of an asset",
+                    description: "Get total value locked of an asset by its ID",
+                    type: "SystemTotalValueLocked",
+                    // TODO write schema
+                    schema: {},
+                })];
+            _totalValueLockedDay_decorators = [resources_1.JsonResources.register({
+                    path: "/systems/{id}/total-value-locked/{year}/{month}/{day}",
+                    summary: "Get total value locked of an asset",
+                    description: "Get total value locked of an asset by its ID",
+                    type: "SystemTotalValueLocked",
+                    // TODO write schema
+                    schema: {},
+                })];
+            _totalValueLockedHour_decorators = [resources_1.JsonResources.register({
+                    path: "/systems/{id}/total-value-locked/{year}/{month}/{day}/{hour}",
+                    summary: "Get total value locked of an asset",
+                    description: "Get total value locked of an asset by its ID",
+                    type: "SystemTotalValueLocked",
+                    // TODO write schema
+                    schema: {},
+                })];
             __esDecorate(_a, null, _index_decorators, { kind: "method", name: "index", static: false, private: false, access: { has: obj => "index" in obj, get: obj => obj.index } }, null, _instanceExtraInitializers);
             __esDecorate(_a, null, _system_decorators, { kind: "method", name: "system", static: false, private: false, access: { has: obj => "system" in obj, get: obj => obj.system } }, null, _instanceExtraInitializers);
             __esDecorate(_a, null, _details_decorators, { kind: "method", name: "details", static: false, private: false, access: { has: obj => "details" in obj, get: obj => obj.details } }, null, _instanceExtraInitializers);
             __esDecorate(_a, null, _icons_decorators, { kind: "method", name: "icons", static: false, private: false, access: { has: obj => "icons" in obj, get: obj => obj.icons } }, null, _instanceExtraInitializers);
             __esDecorate(_a, null, _assets_decorators, { kind: "method", name: "assets", static: false, private: false, access: { has: obj => "assets" in obj, get: obj => obj.assets } }, null, _instanceExtraInitializers);
+            __esDecorate(_a, null, _totalValueLockedHistory_decorators, { kind: "method", name: "totalValueLockedHistory", static: false, private: false, access: { has: obj => "totalValueLockedHistory" in obj, get: obj => obj.totalValueLockedHistory } }, null, _instanceExtraInitializers);
+            __esDecorate(_a, null, _totalValueLockedYear_decorators, { kind: "method", name: "totalValueLockedYear", static: false, private: false, access: { has: obj => "totalValueLockedYear" in obj, get: obj => obj.totalValueLockedYear } }, null, _instanceExtraInitializers);
+            __esDecorate(_a, null, _totalValueLockedMonth_decorators, { kind: "method", name: "totalValueLockedMonth", static: false, private: false, access: { has: obj => "totalValueLockedMonth" in obj, get: obj => obj.totalValueLockedMonth } }, null, _instanceExtraInitializers);
+            __esDecorate(_a, null, _totalValueLockedDay_decorators, { kind: "method", name: "totalValueLockedDay", static: false, private: false, access: { has: obj => "totalValueLockedDay" in obj, get: obj => obj.totalValueLockedDay } }, null, _instanceExtraInitializers);
+            __esDecorate(_a, null, _totalValueLockedHour_decorators, { kind: "method", name: "totalValueLockedHour", static: false, private: false, access: { has: obj => "totalValueLockedHour" in obj, get: obj => obj.totalValueLockedHour } }, null, _instanceExtraInitializers);
         })(),
         _a;
 })();
--- src/api/workers/compile_system.js
+++ src/api/workers/compile_system.js
@@ -1,10 +1,84 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const path_1 = __importDefault(require("path"));
 const worker_threads_1 = require("worker_threads");
 const constants_1 = require("../../constants");
+const csv_1 = require("../../utils/csv");
+const stream_1 = require("../../utils/stream");
+const time_1 = require("../../utils/time");
 const bot_1 = require("../../watcher/bot");
 const systems_1 = require("../resources/systems");
 const compile_1 = require("../utils/compile");
+async function compileHistory(csvName, id, key, createHistoryResource, createYearResource, createMonthResource, createDayResource, createHourResource) {
+    const csvPath = path_1.default.join(constants_1.PATHS.entities, id, constants_1.PATHS.records, csvName);
+    if (!(0, fs_1.existsSync)(csvPath))
+        return;
+    const historyStats = new stream_1.StreamStats(key, 100);
+    const yearsOfHistory = [];
+    let yearsStats;
+    let monthsOfYear = [];
+    let monthsStats;
+    let daysOfMonth = [];
+    let daysStats;
+    let hoursOfDay = [];
+    let hoursStats;
+    let historyObject;
+    async function addHourToDay(hour) {
+        await createHourResource(id, hoursStats?.get());
+        hoursOfDay.push(hour);
+        hoursStats = new stream_1.StreamStats(key, 100);
+    }
+    async function addDayToMonth(day, hour) {
+        await createDayResource(id, daysStats?.get(), yearsOfHistory.at(-1), monthsOfYear.at(-1), daysOfMonth.at(-1), hoursOfDay);
+        await addHourToDay(hour);
+        hoursOfDay = [hour];
+        daysOfMonth.push(day);
+        daysStats = new stream_1.StreamStats(key, 100);
+    }
+    async function addMonthToYear(month, day, hour) {
+        await createMonthResource(id, monthsStats?.get(), yearsOfHistory.at(-1), monthsOfYear.at(-1), daysOfMonth);
+        await addDayToMonth(day, hour);
+        daysOfMonth = [day];
+        monthsOfYear.push(month);
+        monthsStats = new stream_1.StreamStats(key, 100);
+    }
+    async function addYearToHistory(year, month, day, hour) {
+        await createYearResource(id, yearsStats?.get(), yearsOfHistory.at(-1), monthsOfYear);
+        await addMonthToYear(month, day, hour);
+        monthsOfYear = [month];
+        yearsOfHistory.push(year);
+        yearsStats = new stream_1.StreamStats(key, 100);
+    }
+    for await (historyObject of (0, csv_1.readCSV)(csvPath)) {
+        const { year, month, day, hour } = (0, time_1.getDateParts)(historyObject.timestamp);
+        if (yearsOfHistory.at(-1) !== year) {
+            await addYearToHistory(year, month, day, hour);
+        }
+        if (monthsOfYear.at(-1) !== month) {
+            await addMonthToYear(month, day, hour);
+        }
+        if (daysOfMonth.at(-1) !== day) {
+            await addDayToMonth(day, hour);
+        }
+        if (hoursOfDay.at(-1) !== hour) {
+            await addHourToDay(hour);
+        }
+        historyStats.add(historyObject);
+        yearsStats.add(historyObject);
+        monthsStats.add(historyObject);
+        daysStats.add(historyObject);
+        hoursStats.add(historyObject);
+    }
+    if (!yearsOfHistory.length)
+        return;
+    await createHistoryResource(id, historyObject?.timestamp, historyStats.get(), yearsOfHistory);
+    // Finalize by storing last year, month, day, hour
+    await addYearToHistory("N/A", "N/A", "N/A", "N/A");
+}
 worker_threads_1.parentPort?.on("message", async (id) => {
     console.log(`Compile system ${id}`);
     try {
@@ -13,6 +87,7 @@ worker_threads_1.parentPort?.on("message", async (id) => {
             (0, compile_1.compileDetails)(systems_1.SYSTEM_RESOURCES, constants_1.PATHS.systems, id),
             (0, compile_1.compileIcons)(systems_1.SYSTEM_RESOURCES, constants_1.PATHS.systems, id),
             (0, compile_1.compileAssets)(systems_1.SYSTEM_RESOURCES, constants_1.PATHS.systems, id),
+            compileHistory(constants_1.FILES.csv.totalValueLocked, id, "totalValueLocked", systems_1.SYSTEM_RESOURCES.totalValueLockedHistory, systems_1.SYSTEM_RESOURCES.totalValueLockedYear, systems_1.SYSTEM_RESOURCES.totalValueLockedMonth, systems_1.SYSTEM_RESOURCES.totalValueLockedDay, systems_1.SYSTEM_RESOURCES.totalValueLockedHour),
         ]);
         worker_threads_1.parentPort?.postMessage(null);
     }
