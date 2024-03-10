f0078eb76a713eccb5f6a2aa3639aeffac02d17e
Sebastian Penhouet <s.penhouet@outlook.de>
2024-03-10 13:49:03 +0100

    Clean up crawler job

--- src/crawler/job.ts
+++ src/crawler/job.ts
@@ -1,18 +1,18 @@
-import fs from "node:fs/promises";
 import path from "node:path";
+import { getAssetIds, getEntityIds, getSystemIds } from "../api/utils/ids";
 import { job } from "../utils/job";
 import { executeInWorkerPool } from "../utils/worker_pool";
 
 const WORKERS_PATH = "src/crawler/workers";
 
-async function query<Result>(dir: string, workerScript: string): Promise<void> {
-    const workerScriptPath = path.resolve(WORKERS_PATH, workerScript);
-    const ids = await fs.readdir(dir);
-    await executeInWorkerPool<string, Result>(workerScriptPath, ids);
-}
-
 job("Crawler Job", async () => {
-    await query("systems", "query_system.ts");
-    await query("entities", "query_entity.ts");
-    await query("assets", "query_asset.ts");
+    const [assetIds, entityIds, systemIds] = await Promise.all([
+        getAssetIds(),
+        getEntityIds(),
+        getSystemIds(),
+    ]);
+
+    await executeInWorkerPool(path.resolve(WORKERS_PATH, "query_system.ts"), systemIds);
+    await executeInWorkerPool(path.resolve(WORKERS_PATH, "query_entity.ts"), entityIds);
+    await executeInWorkerPool(path.resolve(WORKERS_PATH, "query_asset.ts"), assetIds);
 });
